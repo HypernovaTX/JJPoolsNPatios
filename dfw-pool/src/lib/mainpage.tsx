e940cf9f4f4335223cc6f8a2ab2255a5addf3668
ChengAn (Arthur) Guo <saturnmfgg@gmail.com>
2021-03-30 23:50:11 -0500

    Some file movings and basic edits

---
+++ dfw-pool/src/lib/mainpage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {
+};
+type State = {
+};
+
+export default class MainPage extends React.Component<Props, State> {
+    constructor(p: Props) {
+        super(p);
+        this.state = {};
+    }
+
+    
+}
---
+++ dfw-pool/src/lib/template.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Template {
+    header = (
+        <div></div>
+    );
+}
