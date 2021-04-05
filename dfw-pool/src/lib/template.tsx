e56a979cccddcbdfd6dc027401fd2a5b8e429c4a
ChengAn (Arthur) Guo <saturnmfgg@gmail.com>
2021-04-04 23:55:40 -0500

    Test constructor in case of interactive elements

--- dfw-pool/src/lib/template.tsx
+++ dfw-pool/src/lib/template.tsx
@@ -1,8 +1,14 @@
 export default class Template {
-    header = (
-        <div key='h_'>
-            <div key='ht_'>Test</div>
-        </div>
-    );
+    test: number;
+    constructor(cTest: number) {
+        this.test = cTest;
+    }
+    header(): JSX.Element {
+        return (
+            <div key='h_' className={`TEST${this.test}`}>
+                <div key='ht_'>Test</div>
+            </div>
+        )
+    }
     
 }
