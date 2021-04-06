74a484dfa5be0a47e9b466d19b6fcd96666c6c64
ChengAn (Arthur) Guo <saturnmfgg@gmail.com>
2021-04-06 00:57:10 -0500

    Some html elements and a stock image used for background.

--- dfw-pool/src/lib/template.tsx
+++ dfw-pool/src/lib/template.tsx
@@ -5,8 +5,14 @@ export default class Template {
     }
     header(): JSX.Element {
         return (
-            <div key='h_' className={`TEST${this.test}`}>
-                <div key='ht_'>Test</div>
+            <div key='_h' className='section head'>
+                <div key='_hc' className='section-container'>
+                    <div key='_hcc' className='section-content'>
+                        <div key='_hcc_t' className='section-t head'>JJ Pools n Patios</div>
+                        <div key='_hcc_p' className='section-p head'>About us goes here....lorem ipsum.....etc....etc..</div>
+                        <input key='_hcc_b' type='button' className='section-b head' value='Reach out to us today!'/>
+                    </div>
+                </div>
             </div>
         )
     }
