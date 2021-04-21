087cb8219313810eebdde0f5ec46daebc8e6784c
ChengAn (Arthur) Guo <saturnmfgg@gmail.com>
2021-04-21 00:47:17 -0500

    Some logo adjustments

--- dfw-pool/src/lib/template.tsx
+++ dfw-pool/src/lib/template.tsx
@@ -18,6 +18,7 @@ export default class Template {
         return (
             <div key='_h' className='section head' style={sectionStyle}>
                 <div key='_hc' className='section-container'>
+                    <div key='_hcl' className='section-logo'></div>
                     <div key='_hcc' className='section-content'>
                         <div key='_hcc_t' className='section-t head'>{title}</div>
                         <div key='_hcc_p' className='section-p head'>{summary}</div>
@@ -56,7 +57,6 @@ export default class Template {
     }
 
     public ourServices(): JSX.Element {
-        let outputServices: JSX.Element[] = [];
         // List of services
         const services = [
             {
@@ -80,37 +80,22 @@ export default class Template {
                 price: 650,
             },
         ];
-
-        // Convert the list of services from JSON to list elements
-        services.forEach((part, index) => {
-            let listItem: JSX.Element[] = [];
-            // Save all of the list of services for each of the section
-            part.list.forEach((item) => { listItem.push(<ul>{item}</ul>); })
-            // Save all of the sections of services.
-            outputServices.push(
-                <ul key={`_sv_list${index}`} className='service-list'> 
-                    <ul>{part.title}</ul>
-                    {listItem}
-                    <ul>Starting at ${part.price}</ul> 
-                </ul>
-            );
-        });
+        
+        // Map out the list
+        const output = services.map((s, i) => { return (
+            <div key={`_sv_list${i}`} className='service-list'>
+                <div key={`_sv_li${i}_t`} className='service-list-item'>{s.title}</div>
+                { s.list.map((ss, ii) => { return <div key={`_sv_li${i}_${ii}`} className='service-list-item'>{ss}</div> }) }
+                <div key={`_sv_li${i}_sp`} className='service-list-item spacer'></div>
+                <div key={`_sv_li${i}_p`} className='service-list-item'>Starting at ${s.price}</div>
+            </div>
+        )});
 
         return (
             <div key='_sv_flexbox' className='section-flexbox'>
-                {outputServices}
+                {output}
             </div>
         )
-        /* OLD
-
-        <ul key='_sv_1L' className='service-list'>
-                    <li>Silver Package</li>
-                    <li>Brushing</li>
-                    <li>Emprushing</li>
-                    <li>brushing</li>
-                </ul>
-
-        */
     }
     
 }
