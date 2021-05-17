b7927e0865afc719e8e0a7123c32c2d8df44ec9d
ChengAn (Arthur) Guo <saturnmfgg@gmail.com>
2021-05-17 00:40:19 -0500

    Waves

--- dfw-pool/src/lib/template.tsx
+++ dfw-pool/src/lib/template.tsx
@@ -133,6 +133,9 @@ export default class Template {
         const author = 'Website designed and programmed by Arthur (Hypernova) Guo';
         return(
             <div key='_ft' className='section footer'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <path fill="#0099ff" fill-opacity="1" d="M0,160L60,170.7C120,181,240,203,360,202.7C480,203,600,181,720,170.7C840,160,960,160,1080,154.7C1200,149,1320,139,1380,133.3L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+                </svg>
                 <div key='_ftc' className='section-container'>
                     <span key='_f_text1' className='footer-text'>Copyright &#169;{copyright}</span>
                     <span key='_f_text2' className='footer-text'>{author}</span>
