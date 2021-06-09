35370aec2e936b2e7431dc0590884c07285de4c1
ChengAn (Arthur) Guo <saturnmfgg@gmail.com>
2021-06-09 03:18:36 -0500

    Fixed Emailer.php

--- dfw-pool/emailer.php
+++ dfw-pool/emailer.php
@@ -19,16 +19,19 @@
 	}
 
 	// Define the variables
+	date_default_timezone_set('America/Chicago');
 	$name =      $_POST['name'];
 	$email =     $_POST['email'];
 	$phone =     $_POST['phone'];
 	$title =     $_POST['title'];
 	$message =   $_POST['message'];
 	$in_secret = $_POST['secret'];
+	$my_secret = base64_encode($emailSalt.strval(round(time() / 10))); // $emailSalt is pulled from ./secrets.php
 	$dest =      $toEmail;
+	$date =      date('F j, Y, h:i:s a', time());
 
-	// Check on the hash ($emailSalt is pulled from ./secrets.php)
-	if ($secret !== $emailSalt) {
+	// Check on the hash
+	if ($in_secret !== $my_secret) {
 		echo ('bad secret');
 		exit ('');
 	}
@@ -38,8 +41,16 @@
     $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
     $headers .= "From: " . $email . "\r\n" . "Reply-To: " . $dest . "\r\n" . "X-Mailer: PHP/" . phpversion ();
 
-	// Send the email
-   	$success = mail ($dest, $title, $message, $headers); 
+	// Format the message
+	$messageTo = '<h3 style="color: #0049a8;">You have received an inquiry!</h3><p>';
+	$messageTo .= "<strong>Name:</strong> {$name}<br />";
+	$messageTo .= "<strong>Phone:</strong> {$phone}<br />";
+	$messageTo .= "<strong>Email:</strong> {$email}<br />";
+	$messageTo .= "<strong>Time:</strong> {$date}<br />";
+	$messageTo .= "<strong>Message:</strong><br />{$message}</p>";
+
+	// Send the email to the recipient
+   	$success = mail ($dest, $title, $messageTo, $headers); 
 
 	// Determine success/failure
 	if (!$success) {
